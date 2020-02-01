5207d8319ef44b39c9a6b83a75210f34d5c7c0ff
Eddie Lee <edle@microsoft.com>
2020-01-31 16:50:19 -0800

    delete a model we don't need

--- ActivityFeedNotificationDemo/Microsoft.Teams.Samples.ActivityFeedNotificationDemo/Authorization/AuthModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Microsoft.Teams.Samples.ActivityFeedNotificationDemo.Authorization
-{
-    public class AuthModel
-    {
-        public string GraphAppId { get; set; }
-
-        public string GraphAppSecret { get; set; }
-    }
-}
